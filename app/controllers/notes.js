db7698019d42815e0e4eaed2958d6c41a4da205f
weswhitney <wesleywhitney@gmail.com>
2015-01-30 23:52:52 -0700

    added flash messages

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -6,25 +6,25 @@ export default Ember.ArrayController.extend({
       var body = this.get('noteCopy');
       var title = this.get('noteTitle');
 
-      if (title !== undefined) {
-        if (body !== undefined) {
-          if (title.trim() !== '') {
-            if (body.trim() !== '') {
-              var note = this.store.createRecord('note', {title: title, body: body});
-              note.save();
-              this.set('noteTitle', '');
-              this.set('noteCopy', '');
-            }
-          }
-        }
+      if ((body == null) || (title == null)) {
+        this.flashMessage('fail', 'You cannot have an empty note.');
+      } else {
+        var note = this.store.createRecord('note', {title: title, body: body});
+        this.set('noteTitle', '');
+        this.set('noteCopy', '');
+        note.save().then(function() {
+          this.flashMessage('Note created');
+        }.bind(this));
       }
     },
 
     deleteNote: function (id) {
       this.store.find('note', id).then(function(note) {
         note.deleteRecord();
-        note.save();
-      });
+        note.save().then(function() {
+          this.flashMessage('Deleted!');
+        }.bind(this));
+      }.bind(this));
     }
 
   }
