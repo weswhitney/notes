e26dfc74fe37e74761cf24012bb58e053680793e
weswhitney <wesleywhitney@gmail.com>
2015-01-31 00:03:20 -0700

    CSP

--- config/environment.js
+++ config/environment.js
@@ -12,6 +12,16 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+    
+    ontentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' localhost:3000",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
 
     APP: {
       // Here you can pass flags/options to your application instance
